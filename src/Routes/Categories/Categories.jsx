63198ea4d81fe1334b8883f8910139f667b10eb8
Emiliano Gusberti <emggw@hotmail.com>
2023-07-08 18:29:56 -0300

    Add Categories, items, style

--- src/Routes/Categories/Categories.jsx
+++ src/Routes/Categories/Categories.jsx
@@ -1,4 +1,5 @@
-import useCategories from "./functions/queries";
+import Category from "./components/Category";
+import useCategories from "./Functions/queries";
 import Error from "../../Components/Error";
 import Loader from "../../Components/Loader";
 
@@ -8,13 +9,15 @@ export default function Categories() {
   return (
     <>
       <h1>Categories</h1>
-      <div>
-        {isLoading && <Loader />}
-        {isError && <Error error={error} />}
-        {isSuccess &&
-          categories.map((cat) => {
-            return <p key={cat.id}>{cat.name}</p>;
-          })}
+      <div className="container-fluid text-center">
+        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 row-cols-sm-2">
+          {isLoading && <Loader />}
+          {isError && <Error error={error} />}
+          {isSuccess &&
+            categories.map((cat) => {
+              return <Category key={cat.id} id={cat.id} name={cat.name} image={cat.image} />;
+            })}
+        </div>
       </div>
     </>
   );
