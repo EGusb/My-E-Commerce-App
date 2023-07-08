235d9949d6a151a404c378d7c76c8156844d4c5d
Emiliano Gusberti <emggw@hotmail.com>
2023-07-08 13:26:01 -0300

    Edit Links from Nav into NavLinks

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,18 +1,20 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/register">Sign Up</Link>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Sign Up</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
