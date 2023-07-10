db67b3753826da3303adaa532b9be035bf1f6309
Emiliano Gusberti <emggw@hotmail.com>
2023-07-10 11:56:31 -0300

    Style navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom";
 
+function NavItem({ to, children }) {
+  return (
+    <li className="nav-item p-2">
+      <NavLink to={to}>{children}</NavLink>
+    </li>
+  );
+}
+
 function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Sign Up</NavLink>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-sm">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <NavItem to="/">Home</NavItem>
+          <NavItem to="/login">Login</NavItem>
+          <NavItem to="/register">Sign Up</NavItem>
+        </ul>
+      </div>
     </nav>
   );
 }
