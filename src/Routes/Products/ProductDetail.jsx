fc4f96a8249beddd5212cde8429b76c9dcfcad7a
Emiliano Gusberti <emggw@hotmail.com>
2023-07-10 15:45:24 -0300

    Add detail for Product, style

--- src/Routes/Products/ProductDetail.jsx
+++ src/Routes/Products/ProductDetail.jsx
@@ -1,7 +1,18 @@
+import Error from "../../Components/Error";
+import Loader from "../../Components/Loader";
+import Detail from "./Components/Detail";
+import { getProductDetail } from "../../Functions/queries";
 import { useParams } from "react-router-dom";
 
 export default function ProductDetail() {
   const { id } = useParams();
+  const { data: product, error, isLoading, isError, isSuccess } = getProductDetail(id);
 
-  return <h1>Detail for Product #{id}</h1>;
+  return (
+    <>
+      {isLoading && <Loader />}
+      {isError && <Error error={error} />}
+      {isSuccess && <Detail product={product} />}
+    </>
+  );
 }
