fc987ac5b85f7788ded54954dc7d7f075ba739e3
Emiliano Gusberti <emggw@hotmail.com>
2023-07-13 20:34:32 -0300

    Move list of products to own component

---
+++ src/Routes/Products/Components/ProductList.jsx
@@ -0,0 +1,22 @@
+import ProductCard from "./ProductCard";
+import { getProducts } from "../../../Functions/queries";
+import Error from "../../../Components/Error";
+import Loader from "../../../Components/Loader";
+
+export default function ProductList(props) {
+  const { search } = props; // i.e.: '?categoryId=4&price=820'
+  const { data: products, error, isLoading, isError, isSuccess } = getProducts(search);
+
+  return (
+    <div className="container-fluid text-center p-4">
+      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 row-cols-sm-2">
+        {isLoading && <Loader />}
+        {isError && <Error error={error} />}
+        {isSuccess &&
+          products.map((prod) => {
+            return <ProductCard key={prod.id} product={prod} />;
+          })}
+      </div>
+    </div>
+  );
+}
--- src/Routes/Products/Products.jsx
+++ src/Routes/Products/Products.jsx
@@ -1,30 +1,13 @@
 import { useLocation } from "react-router-dom";
-import Product from "./Components/Product";
-import { getProducts } from "../../Functions/queries";
-import Error from "../../Components/Error";
-import Loader from "../../Components/Loader";
-import { useState } from "react";
+import ProductList from "./Components/ProductList";
 
 export default function Products() {
   const search = useLocation().search; // i.e.: '?categoryId=4&price=820'
-  const [filter, setFilter] = useState(search);
-  const { data: products, error, isLoading, isError, isSuccess } = getProducts(filter);
-
-  filter !== search && setFilter(search);
 
   return (
     <>
       <h1>Products</h1>
-      <div className="container-fluid text-center p-4">
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 row-cols-sm-2">
-          {isLoading && <Loader />}
-          {isError && <Error error={error} />}
-          {isSuccess &&
-            products.map((prod) => {
-              return <Product key={prod.id} product={prod} />;
-            })}
-        </div>
-      </div>
+      <ProductList search={search} />
     </>
   );
 }
