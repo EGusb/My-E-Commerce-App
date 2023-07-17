4796fad6b3dd68c2463d27a9497f3ceb62f6f5cb
Emiliano Gusberti <emggw@hotmail.com>
2023-07-16 22:30:19 -0300

    Remove unnecessary imports

--- src/Routes/Products/Products.jsx
+++ src/Routes/Products/Products.jsx
@@ -1,18 +1,19 @@
-import { useSearchParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import ProductList from "./Components/ProductList";
 import ProductFilter from "./Components/ProductFilter";
 import "./Components/styles.css";
+import { useState } from "react";
 
 export default function Products() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [state, setState] = useState(useLocation().state || {});
 
   return (
     <>
       <h1>Products</h1>
       <div className="container-fluid">
         <div className="row">
-          <ProductFilter searchParams={searchParams} setSearchParams={setSearchParams} />
-          <ProductList searchParams={searchParams} />
+          <ProductFilter state={state} setState={setState} />
+          <ProductList state={state} />
         </div>
       </div>
     </>
