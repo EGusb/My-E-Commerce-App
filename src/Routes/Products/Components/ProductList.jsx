8aeca2e5897e16d995180f5abd093ad8031601a6
Emiliano Gusberti <emggw@hotmail.com>
2023-07-16 21:06:48 -0300

    Fix visual, change var name & getProducts param

--- src/Routes/Products/Components/ProductList.jsx
+++ src/Routes/Products/Components/ProductList.jsx
@@ -4,19 +4,22 @@ import Error from "../../../Components/Error";
 import Loader from "../../../Components/Loader";
 
 export default function ProductList(props) {
-  const { filter } = props; // i.e.: '?categoryId=4&price=820'
-  const { data: products, error, isLoading, isError, isSuccess } = getProducts(filter);
+  const { searchParams } = props;
+  const { data: products, error, isLoading, isError, isSuccess } = getProducts(searchParams.toString());
 
   return (
-    <div className="col-10 p-4">
-      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-sm-2">
-        {isLoading && <Loader />}
-        {isError && <Error error={error} />}
-        {isSuccess &&
-          products.map((prod) => {
-            return <ProductCard key={prod.id} product={prod} />;
-          })}
-      </div>
-    </div>
+    <>
+      <div className="col-2"></div>
+      <main className="col-10 p-4">
+        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-sm-2">
+          {isLoading && <Loader />}
+          {isError && <Error error={error} />}
+          {isSuccess &&
+            products.map((prod) => {
+              return <ProductCard key={prod.id} product={prod} />;
+            })}
+        </div>
+      </main>
+    </>
   );
 }
