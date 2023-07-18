527ba1865a67a47cb850cff4b480148044feff32
Emiliano Gusberti <emggw@hotmail.com>
2023-07-17 23:00:17 -0300

    Pass props

--- src/Routes/Products/Products.jsx
+++ src/Routes/Products/Products.jsx
@@ -5,15 +5,23 @@ import "./Components/styles.css";
 import { useState } from "react";
 
 export default function Products() {
+  const defaultOrderObj = { field: "title", asc: 1 };
   const [state, setState] = useState(useLocation().state || {});
+  const [order, setOrder] = useState(defaultOrderObj);
 
   return (
     <>
       <h1>Products</h1>
       <div className="container-fluid">
         <div className="row">
-          <ProductFilter state={state} setState={setState} />
-          <ProductList state={state} />
+          <ProductFilter
+            state={state}
+            setState={setState}
+            order={order}
+            setOrder={setOrder}
+            defaultOrderObj={defaultOrderObj}
+          />
+          <ProductList state={state} order={order} />
         </div>
       </div>
     </>
