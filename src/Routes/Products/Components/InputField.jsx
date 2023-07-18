555d3a4eda9a82e1d1df9e89218eae16cce6f77b
Emiliano Gusberti <emggw@hotmail.com>
2023-07-17 21:40:58 -0300

    Fix style

--- src/Routes/Products/Components/InputField.jsx
+++ src/Routes/Products/Components/InputField.jsx
@@ -1,6 +1,6 @@
 export default function InputField(props) {
   return (
-    <div className="container-fluid py-3 px-0">
+    <div className="container-fluid py-2 px-0">
       <input
         className="input-field d-block m-auto text-center w-75 rounded-5 border bg-secondary text-black"
         {...props}
