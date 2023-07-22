60bfa82d885c9dce2d5d73daa0b4bb4a30a96fc5
Emiliano Gusberti <emggw@hotmail.com>
2023-07-22 19:19:19 -0300

    Add FilterForm component, edit filter form

--- src/Routes/Products/Components/ProductFilter.jsx
+++ src/Routes/Products/Components/ProductFilter.jsx
@@ -1,6 +1,7 @@
 import { getCategories } from "../../../Functions/queries";
 import { useState, useContext } from "react";
 import { ProductsContext } from "../Context/ProductsContext";
+import FilterForm from "./FilterForm";
 import InputField from "./InputField";
 import SelectField from "./SelectField";
 import objectRemoveBlankValues from "../../../Functions/objectRemoveBlankValues";
@@ -52,64 +53,46 @@ export default function ProductFilter() {
   }
 
   return (
-    <aside className="col-2 position-fixed mt-4 border rounded-5 py-3">
-      <div className="sidebar">
-        <h1 className="py-2">Filters</h1>
-        <form id="filterForm" className="filter-form text-center" onSubmit={handleSubmit} onReset={handleReset}>
-          <SelectField
-            name="orderBy"
-            value={`${orderObj.field}-${orderObj.asc === 1 ? "asc" : "desc"}`}
-            onChange={handleOrderByChange}
-            label="Order By"
-          >
-            <SelectField.Option value="title-asc" content="Title: A-Z" />
-            <SelectField.Option value="title-desc" content="Title: Z-A" />
-            <SelectField.Option value="price-asc" content="Price: Low to High" />
-            <SelectField.Option value="price-desc" content="Price: High to Low" />
-          </SelectField>
+    <FilterForm id="filterForm" title="Filters" onSubmit={handleSubmit} onReset={handleReset}>
+      <SelectField
+        name="orderBy"
+        value={`${orderObj.field}-${orderObj.asc === 1 ? "asc" : "desc"}`}
+        onChange={handleOrderByChange}
+        label="Order By"
+      >
+        <SelectField.Option value="title-asc" content="Title: A-Z" />
+        <SelectField.Option value="title-desc" content="Title: Z-A" />
+        <SelectField.Option value="price-asc" content="Price: Low to High" />
+        <SelectField.Option value="price-desc" content="Price: High to Low" />
+      </SelectField>
 
-          <SelectField name="categoryId" onChange={handleChange} value={`${filterObj.categoryId}`} label="Category">
-            <SelectField.Option key="allCategories" value="" content="All Categories" />
-            {isSuccess &&
-              categories.map((cat) => {
-                return <SelectField.Option key={cat.id} value={cat.id} content={`${cat.id}. ${cat.name}`} />;
-              })}
-          </SelectField>
-          
-          <InputField name="price" type="number" onChange={handleChange} value={filterObj.price} label="Price" />
+      <SelectField name="categoryId" onChange={handleChange} value={`${filterObj.categoryId}`} label="Category">
+        <SelectField.Option key="allCategories" value="" content="All Categories" />
+        {isSuccess &&
+          categories.map((cat) => {
+            return <SelectField.Option key={cat.id} value={cat.id} content={`${cat.id}. ${cat.name}`} />;
+          })}
+      </SelectField>
 
-          <InputField
-            name="price_min"
-            type="number"
-            onChange={handleChange}
-            value={filterObj.price_min}
-            label="Minimum price"
-          />
+      <InputField name="price" type="number" onChange={handleChange} value={filterObj.price} label="Price" />
 
-          <InputField
-            name="price_max"
-            type="number"
-            onChange={handleChange}
-            value={filterObj.price_max}
-            label="Maximum price"
-          />
+      <InputField
+        name="price_min"
+        type="number"
+        onChange={handleChange}
+        value={filterObj.price_min}
+        label="Minimum price"
+      />
 
-          <InputField name="title" type="text" onChange={handleChange} value={filterObj.title} label="Title" />
+      <InputField
+        name="price_max"
+        type="number"
+        onChange={handleChange}
+        value={filterObj.price_max}
+        label="Maximum price"
+      />
 
-          <div className="container-fluid">
-            <div className="row w-75 mx-auto pb-1 pt-3">
-              <button type="submit" className="btn btn-primary col border">
-                Apply filter
-              </button>
-            </div>
-            <div className="row w-75 mx-auto pt-1 pb-3">
-              <button type="reset" className="btn btn-danger col border">
-                Clear
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </aside>
+      <InputField name="title" type="text" onChange={handleChange} value={filterObj.title} label="Title" />
+    </FilterForm>
   );
 }
